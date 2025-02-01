5a6950faad6c77e44518f833555bfdee9f41c674
yanashapka <yana.shapka.test@gmail.com>
2025-02-02 00:44:31 +0200

    task implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css';
+import Card from './components/Card/Card';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+import userData from './userData.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} key={friends.id} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,24 @@
-const Profile = ({ name, tag, location, image, stats }) => {
+import CardStats from '../CardStats/CardStats';
+import css from './Profile.module.css';
+import CardStatsItem from '../CardStatsItem/CardStatsItem';
+import Card from '../Card/Card';
+
+const Profile = ({name, tag, location, image, stats}) => {
   return (
-    <div>
-       {' '}
-      <div>
-           {' '}
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-            <p>Petra Marica</p>    <p>@pmarica</p>    <p>Salvador, Brasil</p> {' '}
+    <Card bigCard={true}>
+      <div className={css.profileWrapper}>
+        <img className={css.profileImage} src={image} alt="User avatar" />
+        <p className={css.profileTextBold}>{name}</p>{' '}
+        <p className={css.profileTextGrey}>@{tag}</p>{' '}
+        <p className={css.profileTextGrey}>{location}</p>
       </div>
-       {' '}
-      <ul>
-           {' '}
-        <li>
-                <span>Followers</span>      <span>1000</span>   {' '}
-        </li>
-           {' '}
-        <li>
-                <span>Views</span>      <span>2000</span>   {' '}
-        </li>
-           {' '}
-        <li>
-                <span>Likes</span>      <span>3000</span>   {' '}
-        </li>
-         {' '}
-      </ul>
-    </div>
+      <CardStats>
+        <CardStatsItem text="Followers" stats={stats.followers}></CardStatsItem>
+        <CardStatsItem text="Views" stats={stats.views}></CardStatsItem>
+        <CardStatsItem text="Likes" stats={stats.likes}></CardStatsItem>
+      </CardStats>
+    </Card>
   );
 };
+
+export default Profile;
